e908435358bbf75aab145ba039bdfe8baefe07cd
armen <armenpetrosan319@gmail.com>
2026-03-18 16:55:57 +0300

    ДДÐВнеÐОпределил структуру проекта и зависимости для сервера

---
+++ backend/src/main/java/com/backend/k_means/KMeansApplication.java
@@ -0,0 +1,13 @@
+package com.backend.k_means;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KMeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KMeansApplication.class, args);
+	}
+
+}
